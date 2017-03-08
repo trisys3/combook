d1a77e1833c3fed5fdc5fa7e4ed2308d2df94e98
Sean Moran <sean_r_moran@msn.com>
2017-03-07 23:00:46 -0500

    Prevented the Stylelint rc file from being ignored by ESLint; preferred single quotes for JSX to be consistent with other JavaScript code & CSS

--- .eslintrc.js
+++ .eslintrc.js
@@ -101,7 +101,7 @@ exports.rules = {
   indent: [2, 2, {
     SwitchCase: 1,
   }],
-  'jsx-quotes': 2,
+  'jsx-quotes': [2, 'prefer-single'],
   'key-spacing': 2,
   'keyword-spacing': [2, {
     overrides: {
