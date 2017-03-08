dc7a12552827a2906ece2af2fb7b79875891d8e3
Sean Moran <sean_r_moran@msn.com>
2017-03-07 23:24:24 -0500

    Received the configuration in the book component that matched what was passed down from the home page & added some hard-coded information for the book used on the home page

--- combook/app.js
+++ combook/app.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+import css from './app.css';
+
 const Page = window.page.default;
 
-export default () => <Page />;
+export default ({book: {shortTitle, author}, pages = 1}) => {
+  const authorName = `${author.first}-${author.last}`;
+  const bookFolder = `./books/${authorName}/${shortTitle}`;
+  const bookCss = require(`${bookFolder}/book.css`);
+
+  if(!Array.isArray(pages)) {
+    pages = [pages];
+  }
+
+  return <div className={`${css.comBook} ${bookCss.comBook}`}>
+    {pages.map((page, index) => {
+      const pageWords = require(`${bookFolder}/pages/${page}/words.txt`);
+      console.log(pageWords);
+      return <Page words={pageWords} key={index} />;
+    })}
+  </div>;
+};
